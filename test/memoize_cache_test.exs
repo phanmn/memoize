593e0236545dc8d68a05f51521b745b1460efc65
giangnt41 <108047851+giangnt41@users.noreply.github.com>
2022-12-02 12:39:14 +0700

    feat: add test for invalidate/0,1 and check if value is stored in :persistent_term (#4)

--- test/memoize_cache_test.exs
+++ test/memoize_cache_test.exs
@@ -195,11 +195,14 @@ defmodule Memoize.CacheTest do
              Memoize.Cache.get_or_run(
                :key,
                fn -> cache_with_call_count(:key, 100) end,
-               back_end: :persistent_term
+               back_end: :persistent_term,
+               expires_in: 1000
              )
 
+    assert {:key, {:completed, 10, _}} = :persistent_term.get(:key)
+
     # # wait to expire the cache
-    Process.sleep(1200)
+    Process.sleep(1500)
 
     assert 20 ==
              Memoize.Cache.get_or_run(
@@ -207,6 +210,8 @@ defmodule Memoize.CacheTest do
                fn -> cache_with_call_count(:key, 100) end,
                back_end: :persistent_term
              )
+
+    assert {:key, {:completed, 20, _}} = :persistent_term.get(:key)
   end
 
   @tag cache: "default"
@@ -315,6 +320,9 @@ defmodule Memoize.CacheTest do
                back_end: :persistent_term
              )
 
+    assert {:key1, {:completed, 10, _}} = :persistent_term.get(:key1)
+    assert {:key3, {:completed, 10, _}} = :persistent_term.get(:key3)
+
     # wait to expire the cache
     Process.sleep(120)
     # insert new value
@@ -326,8 +334,12 @@ defmodule Memoize.CacheTest do
                back_end: :persistent_term
              )
 
+    assert {:key2, {:completed, 10, _}} = :persistent_term.get(:key2)
+
     # :key1's value is collected
     assert 2 == Memoize.Cache.garbage_collect()
+
+    assert [] == :persistent_term.get(:key1, [])
 
     assert 20 ==
              Memoize.Cache.get_or_run(
@@ -336,6 +348,8 @@ defmodule Memoize.CacheTest do
                expires_in: 100,
                back_end: :persistent_term
              )
+
+    assert {:key1, {:completed, 20, _}} = :persistent_term.get(:key1)
 
     assert 10 ==
              Memoize.Cache.get_or_run(
@@ -349,6 +363,71 @@ defmodule Memoize.CacheTest do
              Memoize.Cache.get_or_run(:key3, fn -> cache_with_call_count(:key3, 0) end,
                back_end: :persistent_term
              )
+  end
+
+  @tag cache: "default"
+  test "invalidate/0 and invalidate/1 for pesistent_term" do
+    assert 10 ==
+             Memoize.Cache.get_or_run(
+               :key1,
+               fn -> cache_with_call_count(:key1, 100) end,
+               expires_in: 100,
+               back_end: :persistent_term
+             )
+
+    assert 10 ==
+             Memoize.Cache.get_or_run(:key3, fn -> cache_with_call_count(:key3, 0) end,
+               back_end: :persistent_term
+             )
+
+    assert {:key1, {:completed, 10, _}} = :persistent_term.get(:key1)
+    assert {:key3, {:completed, 10, _}} = :persistent_term.get(:key3)
+
+    # invalidate/0
+    Memoize.CacheStrategy.Default.invalidate()
+
+    assert [] == :persistent_term.get(:key1, [])
+    assert [] == :persistent_term.get(:key3, [])
+
+    # insert new value
+    assert 10 ==
+             Memoize.Cache.get_or_run(
+               :key2,
+               fn -> cache_with_call_count(:key2, 0) end,
+               expires_in: 100,
+               back_end: :persistent_term
+             )
+
+    assert {:key2, {:completed, 10, _}} = :persistent_term.get(:key2)
+
+    assert 20 ==
+             Memoize.Cache.get_or_run(
+               :key1,
+               fn -> cache_with_call_count(:key1, 0) end,
+               expires_in: 100,
+               back_end: :persistent_term
+             )
+
+    assert {:key1, {:completed, 20, _}} = :persistent_term.get(:key1)
+
+    assert 10 ==
+             Memoize.Cache.get_or_run(
+               :key2,
+               fn -> cache_with_call_count(:key2, 0) end,
+               expires_in: 100,
+               back_end: :persistent_term
+             )
+
+    assert 20 ==
+             Memoize.Cache.get_or_run(:key3, fn -> cache_with_call_count(:key3, 0) end,
+               back_end: :persistent_term
+             )
+
+    # invalidate/1
+    Memoize.CacheStrategy.Default.invalidate(:key2)
+    assert [] == :persistent_term.get(:key2, [])
+    assert {:key1, {:completed, 20, _}} = :persistent_term.get(:key1)
+    assert {:key3, {:completed, 20, _}} = :persistent_term.get(:key3)
   end
 
   def eat_memory(threshold) do
